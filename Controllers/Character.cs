baa8209bbb16c5f9d330ac8913bb0f69c68cb2c4
Ricardo Varjão <rvarjao@gmail.com>
2024-06-14 10:45:54 -0700

    Add project files.

---
+++ Controllers/Character.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Marvel.Controllers
+{
+    public class Character : Controller
+    {
+        public string Index()
+        {
+            return "view character";
+        }
+    }
+}

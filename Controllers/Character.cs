484a2e830a0a77986d2e8b8f02bd6c0de38f4263
Ricardo Varjão <rvarjao@gmail.com>
2024-06-15 23:19:22 -0700

    Pagination
    Still have some bug to fix in the pagination, but the links are working

--- Controllers/Character.cs
+++ Controllers/Character.cs
@@ -30,8 +30,7 @@ namespace Marvel.Controllers
 
             string allCharacter = characters.Get().Result;
             Models.Marvel.APIReturn? aPIReturn = System.Text.Json.JsonSerializer.Deserialize<Models.Marvel.APIReturn>(allCharacter);
-            List<Models.Marvel.Character>? charactersList = aPIReturn?.data.results;
-            return View(charactersList);
+            return View(aPIReturn.data);
         }
     }
 }
